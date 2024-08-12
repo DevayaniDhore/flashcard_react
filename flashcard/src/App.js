8d076656f4e2bf2a108f038620beaa9eed3eae9a
ashishck27 <162847929+ashishck27@users.noreply.github.com>
2024-08-13 01:06:04 +0530

    initial update

---
+++ flashcard/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import FlashcardView from './Componant/FlashcardView';
+import Dashboard from './Componant/Dashboard';
+
+
+function App() {
+  return (
+    <div>
+      <FlashcardView></FlashcardView>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
